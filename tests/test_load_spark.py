32458e74b80653d2bbaf7126f4dd19988569ba1c
yennanliu <f339339@gmail.com>
2020-04-19 18:13:06 +0800

    fix code layout (test_load_spark.py), update gitignore

--- tests/test_load_spark.py
+++ tests/test_load_spark.py
@@ -5,9 +5,9 @@ from utils import init_spark
 
 
 def test_load_spark():
-	init_s = init_spark.InitSpark()
-	#print ("****", str(init_s.sc))
-	assert str(init_s.sc) == "<SparkContext master=local[*] appName=pyspark-shell>"
+    init_s = init_spark.InitSpark()
+    #print ("****", str(init_s.sc))
+    assert str(init_s.sc) == "<SparkContext master=local[*] appName=pyspark-shell>"
 
 if __name__ == '__main__':
-	pytest.main([__file__])
+    pytest.main([__file__])
